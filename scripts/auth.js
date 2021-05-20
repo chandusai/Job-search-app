db6499fc654273f4ae8589a61ac8a251f3453461
chandu movva <chandumovva4@gmail.com>
2021-05-21 10:16:43 +1200

    updated changes

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,28 +1,8 @@
 // Tracking auth state
 
 auth.onAuthStateChanged(user=>{
-  if(user){
-    db.collection('Jobs').orderBy('Date').onSnapshot(snapshot=>{
-      let jobs= snapshot.docChanges()
-      console.log(jobs)
-      jobs.forEach(job=>{
-          if(job.type =='added'){
-            renderjobs(job.doc)
-          }
-          else if(job.type == 'removed'){            
-            const list= document.querySelector('.job__list')
-             let mn =   list.querySelector(`[data-id = ${job.doc.id}]`)
-             list.removeChild(mn)
-             UI.showAlert("You successfully deleted the listing",'primary')
-           }
-          
-      })
-  })
-  }
-  else{
-    
-    UI.showAlert("Please login/signup in to your account",'primary')
-  }
+  
+ 
 })
 
 
